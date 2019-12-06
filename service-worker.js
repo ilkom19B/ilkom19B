73f43ca25d8c415b79c0b7dd84513790443e0b4c
ilkom19B <ilkom19B@gmail.com>
2019-12-06 14:21:12 +0700

    menambah api

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,9 @@ var urlsToCache = [
 	"/css/materialize.min.css",
 	"/css/style.css",
 	"/js/materialize.min.js",
-	"/js/nav.js"
+	"/js/nav.js",
+	"/js/api.js",
+	"/article.html"
 ];
 
 self.addEventListener("install", function (event) {
@@ -27,6 +29,31 @@ self.addEventListener("install", function (event) {
 		})
 	);
 });
+
+// api
+self.addEventListener("fetch", function (event) {
+	var base_url = "https://readerapi.codepolitan.com/";
+
+	if (event.request.url.indexOf(base_url) > -1) {
+		event.respondWith(
+			caches.open(CACHE_NAME).then(function (cache) {
+				return fetch(event.request).then(function (response) {
+					cache.put(event.request.url, response.clone());
+					return response;
+				})
+			})
+		)
+	} else {
+		event.respondWith(
+			caches.match(event.request, {
+				ignoreSearch: true
+			}).then(function (response) {
+				return response || fetch(event.request);
+			})
+		)
+	}
+});
+// akhir api
 
 self.addEventListener("fetch", function (event) {
 	event.respondWith(
